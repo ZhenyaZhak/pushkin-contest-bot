699850ce80a502321f7fc2dcf1987ab8b31b46cc
ZhenyaZhak <zhenya-zhak@mail.ru>
2017-12-27 21:50:59 +0300

    some test

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'test/test'
-  root 'quiz#index'
-  #resources :quiz, only: [:index]
-
   post 'quiz', to: 'quiz#quiz'
+  root 'quiz#index'
+  get 'test/test'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
