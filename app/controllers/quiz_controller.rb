3eb9c8dd811f1317dafb0a47b8da1c0c6982d8a2
ZhenyaZhak <zhenya-zhak@mail.ru>
2017-12-27 21:29:27 +0300

    some test

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -11,32 +11,36 @@ class QuizController < ApplicationController
   	s_file = File.read('pushkin.json')
 		str = JSON.parse(s_file)
 		answer = ""
-		inp_s = params[:question]
-		file = File.open('in_data.json', 'w') do |f|
-		  f.write(inp_s.to_json)
-		end
-		fl = 0
-  	if 1 == params[:level].to_i
+		question = params["question"]
+		level = params["level"].to_i
+		id = params["id"]
+		question = question.gsub!(/[\«\»\~\!\@\#\$\%\^\&\*\(\)\_\+\`\-\=\№\;\?\/\,\.\/\;\'\\\|\{\}\:\"\[\]\<\>\?\—]/,"")
+		question = question.strip
+		#fl = 0
+  	if level == 1
   		str.map do |e|
-				tmp_str = e[1].split("\n")
-				tmp_str.map do |el|
-					if el == inp_s
-						answer = e[0]
-						fl = 1
-						break
-					end
-				end
-				if fl == 1
-					break
-				end
+  			tmp_str = e[1].gsub!(/[\«\»\~\!\@\#\$\%\^\&\*\(\)\_\+\`\-\=\№\;\?\/\,\.\/\;\'\\\|\{\}\:\"\[\]\<\>\?\—]/,"")
+  			if tmp_str.include?(question)
+  				answer = e[0]
+  				break
+  			end
+				#tmp_str = e[1].split("\n")
+				#tmp_str.map do |el|
+				#	if el == inp_s
+				#		answer = e[0]
+				#		fl = 1
+				#		break
+				#	end
+				#end
+				#if fl == 1
+				#	break
+				#end
 			end
-  		puts 'ok'
-  		puts params
   	end
   	parameters = {
 		  answer: answer,
 		  token: "60ecace79d6a948133f9fbcd7a0a4df4",
-		  task_id:  params[:id]
+		  task_id: id
 		}
 		Net::HTTP.post_form(APP_URI, parameters)
   end
