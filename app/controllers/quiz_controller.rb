39ea85a5238be71cac0074be8664c63149e8b17e
ZhenyaZhak <zhenya-zhak@mail.ru>
2017-12-29 14:26:21 +0300

    some test

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -8,6 +8,7 @@ class QuizController < ApplicationController
   end
 
   def task
+    start_time = Time.now
     s_file = File.read('pushkin.json')
     str = JSON.parse(s_file)
     answer = ""
@@ -38,12 +39,20 @@ class QuizController < ApplicationController
         token: '60ecace79d6a948133f9fbcd7a0a4df4',
         task_id: id
       }
+      p1_time = Time.now
       res = Net::HTTP.post_form(uri_app, parameters)
       render json: 'ok'
       puts res.body
+      p2_time = Time.now
     end
+    time1 = p1_time - start_time
+    time2 = p2_time - start_time
+    par = {
+      time1: time1
+      time2: time2
+    }
     file = File.open('in_data.json', 'w') do |f|
-      f.write(parameters)
+      f.write(par)
     end
     #file = File.open('in_data.json', 'w') do |f|
     #  f.write(check.to_json)
